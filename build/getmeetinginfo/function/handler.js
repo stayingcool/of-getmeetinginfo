3f756fd90d79351303cfb759eeaed37c0e680e2b
NISAR AHMED <stayingcool@gmail.com>
2018-09-16 21:39:31 +0530

    Gateway and image

--- build/getmeetinginfo/function/handler.js
+++ build/getmeetinginfo/function/handler.js
@@ -2,9 +2,9 @@
 
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: 'my-release-mysql.default.svc.cluster.local',
+    host: 'mysql.default.svc.cluster.local',
     user: 'root',
-    password: 'IA3e8rdnqO',
+    password: 'password',
     database: 'attendancedb'
 });
 
